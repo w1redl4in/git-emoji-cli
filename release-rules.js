97a6fc460ede015f4f004c22688c9a329f1f1e73
Felipe <felipe.araujo@hubfintech.com.br>
2022-03-18 20:29:25 -0300

    feat(semantic-release): :sparkles: add style scope

--- release-rules.js
+++ release-rules.js
@@ -1,7 +1,8 @@
 module.exports = [
-  { type: "docs", scope: "", release: "patch" },
   { type: "refactor", scope: "", release: "minor" },
   { type: "feat", scope: "", release: "minor" },
   { type: "chore", scope: "", release: "minor" },
+  { type: "style", scope: "", release: "minor" },
+  { type: "docs", scope: "", release: "patch" },
   { type: "fix", scope: "", release: "patch" },
 ];
