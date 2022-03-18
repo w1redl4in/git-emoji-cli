8bb1e1acc7787b3b44a8fe1e3d39c4f04631ce2f
Felipe <felipe.araujo@hubfintech.com.br>
2022-03-18 20:24:46 -0300

    chore: :rocket: release rules for semantic-release

---
+++ release-rules.js
@@ -0,0 +1,7 @@
+module.exports = [
+  { type: "docs", release: "patch" },
+  { type: "refactor", release: "minor" },
+  { type: "feat", release: "minor" },
+  { type: "chore", release: "minor" },
+  { type: "fix", release: "patch" },
+];
