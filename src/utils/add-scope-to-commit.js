12c77652805411cef5d79467d430478b73edf953
Felipe <felipe.araujo@hubfintech.com.br>
2022-03-19 10:35:02 -0300

    fix(add-scope-to-commit): :ambulance: re adding emoji to commit, the most important one

--- src/utils/add-scope-to-commit.js
+++ src/utils/add-scope-to-commit.js
@@ -1,9 +1,9 @@
 function addScopeToCommit(commitType, scope, input) {
-  const [semanticCommit] = commitType.split(" ");
+  const [semanticCommit, emoji] = commitType.split(" ");
 
   const semanticCommitWithScope = semanticCommit.replace(
     ":",
-    `(${scope}): ${input}`
+    `(${scope}): ${emoji} ${input}`
   );
 
   return semanticCommitWithScope;
